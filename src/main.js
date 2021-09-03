e75a1f6d786babd005c8a346b768a052cade8e46
Tatsuya Katsuhara <tkatsuha@amazon.com>
2021-09-03 15:15:49 +0900

    Congnitoとの最低限のFederationするところまで構成

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 import {
@@ -9,3 +11,7 @@ Amplify.configure(aws_exports);
 applyPolyfills().then(() => {
   defineCustomElements(window);
 });
+
+const app = createApp(App);
+app.config.isCustomElement = tag => tag.startsWith('amplify-');
+app.mount('#app')
